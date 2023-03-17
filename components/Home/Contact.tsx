c9795970603a946996e6eb55e9e32006854843fa
unknown <ruthani124@gmail.com>
2023-03-17 04:57:48 +0100

    special offers

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -21,7 +21,7 @@ export const ContactUs: FC = () => {
     //`${process.env.NEXT_PUBLIC_BACKEND_HOST}web/contact-us/create
     const mutation = useMutation(
         (contactDetails : any) => {
-            return axios.post(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/web/contact-us/create`, contactDetails,{
+            return axios.post(`${process.env.NEXT_PUBLIC_BACKEND_HOST}web/contact-us/create`, contactDetails,{
                headers:{
                 'Content-Type' : 'application/json',
                 'Authorization' : `Bearer ${process.env.NEXT_PUBLIC_USER_TOKEN}`
