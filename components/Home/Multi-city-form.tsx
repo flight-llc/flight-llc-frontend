d29589d84142d4c0087a0d530940d2c4883e9d34
unknown <ruthani124@gmail.com>
2023-04-09 10:27:40 +0100

    menu fix

--- components/Home/Multi-city-form.tsx
+++ components/Home/Multi-city-form.tsx
@@ -183,7 +183,7 @@ export const MultiCityForm: FC<props> = ({ bookFlight, locations, setTimer }) =>
             
             <div className='w-full'>
                 {/* <form onSubmit={onSubmitMultiCityFlights}> */}
-                <div className='max-h-[13rem] bg-blue-800 overflow-y-auto mt-4 rounded-lg' style={{ overflowY: 'auto' }}>
+                <div className='max-h-[12rem] overflow-y-auto mt-4 rounded-lg' style={{ overflowY: 'auto' }}>
                     {fields && fields.map((fieldData: any, i: number) =>
                         <div key={`key_${i}`} className='flex flex-row items-center gap-4'>
                             <div className="w-full flex p-3 bg-white mb-3 rounded-lg">
